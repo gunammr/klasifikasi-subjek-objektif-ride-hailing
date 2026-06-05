0162b85dbd013e400fe5de6187e5a1a72b1520dd
Kaa-n <tanjakanijo@gmail.com>
2026-06-05 17:12:21 +0800

    tf-idf-uji

--- tf-idf.py
+++ tf-idf.py
@@ -1,14 +1,18 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
-dokumen = [
-    "Kucing itu tidur di atas meja.",
-    "Anjing itu mengejar kucing di taman.",
-    "Kucing dan anjing adalah hewan peliharaan."
-]
 
-# 2. Inisialisasi model TF-IDF
+df = pd.read_csv('stemmed_reviews.csv')
 
+kolom_target = 'stemmed_review' 
+
+# Jika ada baris kosong (NaN), paksa menjadi string kosong
+df[kolom_target] = df[kolom_target].fillna('')
+
+# Ubah kolom DataFrame kembali menjadi format list yang dipahami TfidfVectorizer
+dokumen = df[kolom_target].tolist()
+
+# Inisialisasi model TF-IDF
 vectorizer = TfidfVectorizer(lowercase=False)
 
 # Fit (pelajari kosakata) dan Transform (hitung bobot TF-IDF)
@@ -20,8 +24,7 @@ kata_kunci = vectorizer.get_feature_names_out()
 # Ubah hasil (sparse matrix) menjadi array biasa dan masukkan ke DataFrame Pandas
 df_tfidf = pd.DataFrame(tfidf_matrix.toarray(), columns=kata_kunci)
 
-print("DAFTAR KATA (VOCABULARY)")
-print(kata_kunci)
+print(f"Berhasil memproses {len(dokumen)} dokumen.")
 
-print("\nMATRIKS TF-IDF")
-print(df_tfidf)
+print("\n== MATRIKS TF-IDF 5 Baris Pertama ==")
+print(df_tfidf.head())
